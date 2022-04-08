5f3befb92bfc94295c4e3dc273b999bc1c21b575
Seremet Alexandru <andutzu.268@gmail.com>
2022-04-08 13:29:59 +0300

    sxdsaas

--- SDA/Chihai/5/task_1/main.c
+++ SDA/Chihai/5/task_1/main.c
@@ -8,6 +8,7 @@ int main()
     create_int_list();
     list = toArr();
     int_to_str_list(list, count_int());
+    printf("\nlista ce contine elemente convertite din int decimal in str binar:\n");
     show_str_list();
     return (0);
 }
